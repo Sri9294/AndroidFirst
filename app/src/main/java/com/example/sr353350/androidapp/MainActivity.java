7576b30fc4f2ab03506d8c32c6a996630f661af9
Sri9294 <Sridevi2359@gmail.com>
2017-10-27 19:44:31 +0530

    Hello world

--- app/src/main/java/com/example/sr353350/androidapp/MainActivity.java
+++ app/src/main/java/com/example/sr353350/androidapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+/* As per the document ,I think this POC is all about learning how to build default android application
+   and to print hello world on the screen.Hence I have not implemented anything in Mainactivity.java,
+   a text field representing HELLO WORLD is added in activity_main layout */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
